2f6b232a523146f3ed8f58e7a9db122120d1a897
Худойкулов Суннат <khudoykulov07@bk.ru>
2024-11-15 21:15:07 +0500

    Все работает верно? lesson04

--- script.js
+++ script.js
@@ -1,48 +1,14 @@
 let title = prompt("Как называется ваш проект?");
-let screens = prompt("Какие типы экранов нужно разработать?");
-let screenPrice = +prompt("Сколько будет стоить данная работа?");
-let adaptive = confirm("Нужен ли адаптив на сайте?");
-let servicePrice1 = +prompt("Сколько это будет стоить?");
-let servicePrice2 = +prompt("Сколько это будет стоить?");
+let screens = prompt("Какие типы экранов нужно разработать?", "Простые");
+let screenPrice = +prompt("Сколько будет стоить данная работа?", "20000");
+let adaptive = confirm("Нужен ли адаптив на сайте?", "Нет");
+let service1 = prompt('Какой дополнительный тип услуг нужен?', "нет");
+let service2 = prompt('Какой дополнительный тип услуг нужен?', "нет");
+let servicePrice1 = +prompt("Сколько это будет стоить?", "2000");
+let servicePrice2 = +prompt("Сколько это будет стоить?", "2000");
 let rollback = 10;
 
-// Вывод типов переменных
-const showTypeOf = function (variable) {
-	console.log(variable, typeof variable);
-}
-showTypeOf(title);
-showTypeOf(screenPrice);
-showTypeOf(adaptive);
-
-// Расчет общих цен услуг
-const getAllServicePrices = function (ser1, ser2) {
-	return ser1 + ser2;
-}
-let allServicePrices = getAllServicePrices(servicePrice1, servicePrice2);
-
-// Расчет полной стоимости
-function getFullPrice(screenPrice, allServicePrices) {
-	return screenPrice + allServicePrices;
-}
-let fullPrice = getFullPrice(screenPrice, allServicePrices);
-
-// Функция для получения преобразованного title
-function getTitle(title) {
-	let trimmedTitle = title.trim(); // Удаляем пробелы с начала и конца
-	return trimmedTitle[0].toUpperCase() + trimmedTitle.substring(1).toLowerCase();
-}
-
-// Расчет стоимости за вычетом отката
-function getServicePercentPrices(fullPrice, rollback) {
-	return fullPrice * (1 - rollback / 100);
-}
-let servicePercentPrice = getServicePercentPrices(fullPrice, rollback);
-
-// Вывод строк с типами экранов
-console.log(screens);
-
-// Вывод сообщения о скидке пользователю
-function getRollBackMessage(price) {
+const getRollBackMessage = function (price) {
 	if (price >= 30000) {
 		return "Даём скидку в 10%";
 	} else if (price >= 15000 && price < 30000) {
@@ -53,10 +19,52 @@ function getRollBackMessage(price) {
 		return "Что-то пошло не так";
 	}
 }
-console.log(getRollBackMessage(fullPrice));
 
-// Стоимость за вычетом процента отката посреднику
-console.log(servicePercentPrice);
+const showTypeOf = function (variable) {
+	console.log(variable, typeof variable);
+}
 
-// Проверка и вывод преобразованного title
-console.log(getTitle(title));
+const getAllServicePrices = function (ser1, ser2) {
+	return ser1 + ser2
+}
+const allServicePrices = getAllServicePrices(servicePrice1, servicePrice2);
+
+function getFullPrice(elem1, elem2) {
+	return elem1 + elem2
+}
+
+const fullPrice = getFullPrice(screenPrice, allServicePrices)
+let amountKickback = (fullPrice * (rollback / 100));
+
+function getTitle(elem) {
+	if (elem !== '') {
+		let lowerLetters = elem.toLowerCase();
+		let num = 0;
+		while (lowerLetters[num] == ' ') {
+			num += 1;
+		}
+		let firstLetter = lowerLetters[num].toUpperCase();
+		let lowerLettersSlice = lowerLetters.slice(num + 1);
+		let result = firstLetter + lowerLettersSlice;
+		return result;
+	} else {
+		return "Название проекта отсутствует";
+	}
+}
+let getTitleLet = getTitle(title)
+
+function getServicePercentPrices(elem1, elem2) {
+	return elem1 - elem2
+}
+let servicePercentPrice = getServicePercentPrices(fullPrice, amountKickback)
+
+showTypeOf(getTitleLet)
+showTypeOf(screenPrice)
+// showTypeOf(adaptive)
+// showTypeOf(service1)
+// showTypeOf(service2)
+showTypeOf(servicePrice1)
+showTypeOf(servicePrice2)
+showTypeOf(rollback)
+showTypeOf(servicePercentPrice)
+showTypeOf(getRollBackMessage(fullPrice))
