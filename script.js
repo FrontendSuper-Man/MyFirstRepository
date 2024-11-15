e9b4d7de99080c037ef0e62b970414e140c9ff02
Худойкулов Суннат <khudoykulov07@bk.ru>
2024-11-15 18:19:53 +0500

    Другой вариант..

--- script.js
+++ script.js
@@ -2,17 +2,47 @@ let title = prompt("Как называется ваш проект?");
 let screens = prompt("Какие типы экранов нужно разработать?");
 let screenPrice = +prompt("Сколько будет стоить данная работа?");
 let adaptive = confirm("Нужен ли адаптив на сайте?");
-let service1 = prompt('Какой дополнительный тип услуг нужен?');
-let service2 = prompt('Какой дополнительный тип услуг нужен?');
 let servicePrice1 = +prompt("Сколько это будет стоить?");
 let servicePrice2 = +prompt("Сколько это будет стоить?");
 let rollback = 10;
 
+// Вывод типов переменных
 const showTypeOf = function (variable) {
 	console.log(variable, typeof variable);
 }
+showTypeOf(title);
+showTypeOf(screenPrice);
+showTypeOf(adaptive);
 
-const getRollBackMessage = function (price) {
+// Расчет общих цен услуг
+const getAllServicePrices = function (ser1, ser2) {
+	return ser1 + ser2;
+}
+let allServicePrices = getAllServicePrices(servicePrice1, servicePrice2);
+
+// Расчет полной стоимости
+function getFullPrice(screenPrice, allServicePrices) {
+	return screenPrice + allServicePrices;
+}
+let fullPrice = getFullPrice(screenPrice, allServicePrices);
+
+// Функция для получения преобразованного title
+function getTitle(title) {
+	let trimmedTitle = title.trim(); // Удаляем пробелы с начала и конца
+	return trimmedTitle[0].toUpperCase() + trimmedTitle.substring(1).toLowerCase();
+}
+
+// Расчет стоимости за вычетом отката
+function getServicePercentPrices(fullPrice, rollback) {
+	return fullPrice * (1 - rollback / 100);
+}
+let servicePercentPrice = getServicePercentPrices(fullPrice, rollback);
+
+// Вывод строк с типами экранов
+console.log(screens);
+
+// Вывод сообщения о скидке пользователю
+function getRollBackMessage(price) {
 	if (price >= 30000) {
 		return "Даём скидку в 10%";
 	} else if (price >= 15000 && price < 30000) {
@@ -23,44 +53,10 @@ const getRollBackMessage = function (price) {
 		return "Что-то пошло не так";
 	}
 }
-
-showTypeOf(title)
-showTypeOf(screenPrice)
-showTypeOf(adaptive)
-
-const getAllServicePrices = function (ser1, ser2) {
-	return ser1 + ser2
-}
-const allServicePrices = getAllServicePrices(servicePrice1, servicePrice2);
-
-function getFullPrice(elem1, elem2) {
-	return elem1 + elem2
-}
-let fullPrice = getFullPrice(screenPrice, allServicePrices)
-let amountKickback = (fullPrice * (rollback / 100));
-
-function getTitle(elem) {
-	if (elem !== '') {
-		let lowerLetters = elem.toLowerCase();
-		let num = 0;
-		while (lowerLetters[num] == ' ') {
-			num += 1;
-		}
-		let firstLetter = lowerLetters[num].toUpperCase();
-		let lowerLettersSlice = lowerLetters.slice(num + 1);
-		let result = firstLetter + lowerLettersSlice;
-		return result;
-	} else {
-		return "Название проекта отсутствует";
-	}
-}
-
-function getServicePercentPrices(elem1, elem2) {
-	return elem1 - elem2
-}
-servicePercentPrice = getServicePercentPrices(fullPrice, amountKickback)
-
-
-console.log(servicePercentPrice);
 console.log(getRollBackMessage(fullPrice));
-console.log(typeof screens, screens);
+
+// Стоимость за вычетом процента отката посреднику
+console.log(servicePercentPrice);
+
+// Проверка и вывод преобразованного title
+console.log(getTitle(title));
