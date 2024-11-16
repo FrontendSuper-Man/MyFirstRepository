f606842edce9fdea32a102546ddf72c3a92b2413
Худойкулов Суннат <khudoykulov07@bk.ru>
2024-11-16 19:15:43 +0500

    Помеянл все let на const lesson04

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-let title = prompt("Как называется ваш проект?");
-let screens = prompt("Какие типы экранов нужно разработать?", "Простые");
-let screenPrice = +prompt("Сколько будет стоить данная работа?", "20000");
-let adaptive = confirm("Нужен ли адаптив на сайте?", "Нет");
-let service1 = prompt('Какой дополнительный тип услуг нужен?', "нет");
-let service2 = prompt('Какой дополнительный тип услуг нужен?', "нет");
-let servicePrice1 = +prompt("Сколько это будет стоить?", "2000");
-let servicePrice2 = +prompt("Сколько это будет стоить?", "2000");
-let rollback = 10;
+const title = prompt("Как называется ваш проект?");
+const screens = prompt("Какие типы экранов нужно разработать?", "Простые");
+const screenPrice = +prompt("Сколько будет стоить данная работа?", "20000");
+const adaptive = confirm("Нужен ли адаптив на сайте?", "Нет");
+const service1 = prompt('Какой дополнительный тип услуг нужен?', "нет");
+const service2 = prompt('Какой дополнительный тип услуг нужен?', "нет");
+const servicePrice1 = +prompt("Сколько это будет стоить?", "2000");
+const servicePrice2 = +prompt("Сколько это будет стоить?", "2000");
+const rollback = 10;
 
 const getRollBackMessage = function (price) {
 	if (price >= 30000) {
@@ -25,46 +25,46 @@ const showTypeOf = function (variable) {
 }
 
 const getAllServicePrices = function (ser1, ser2) {
-	return ser1 + ser2
+	return ser1 + ser2;
 }
 const allServicePrices = getAllServicePrices(servicePrice1, servicePrice2);
 
-function getFullPrice(elem1, elem2) {
-	return elem1 + elem2
+const getFullPrice = function (elem1, elem2) {
+	return elem1 + elem2;
 }
 
-const fullPrice = getFullPrice(screenPrice, allServicePrices)
-let amountKickback = (fullPrice * (rollback / 100));
+const fullPrice = getFullPrice(screenPrice, allServicePrices);
+const amountKickback = (fullPrice * (rollback / 100));
 
-function getTitle(elem) {
+const getTitle = function (elem) {
 	if (elem !== '') {
-		let lowerLetters = elem.toLowerCase();
+		const lowerLetters = elem.toLowerCase();
 		let num = 0;
-		while (lowerLetters[num] == ' ') {
+		while (lowerLetters[num] === ' ') {
 			num += 1;
 		}
-		let firstLetter = lowerLetters[num].toUpperCase();
-		let lowerLettersSlice = lowerLetters.slice(num + 1);
-		let result = firstLetter + lowerLettersSlice;
+		const firstLetter = lowerLetters[num].toUpperCase();
+		const lowerLettersSlice = lowerLetters.slice(num + 1);
+		const result = firstLetter + lowerLettersSlice;
 		return result;
 	} else {
 		return "Название проекта отсутствует";
 	}
 }
-let getTitleLet = getTitle(title)
+const getTitleLet = getTitle(title);
 
-function getServicePercentPrices(elem1, elem2) {
-	return elem1 - elem2
+const getServicePercentPrices = function (elem1, elem2) {
+	return elem1 - elem2;
 }
-let servicePercentPrice = getServicePercentPrices(fullPrice, amountKickback)
+const servicePercentPrice = getServicePercentPrices(fullPrice, amountKickback);
 
-showTypeOf(getTitleLet)
-showTypeOf(screenPrice)
-// showTypeOf(adaptive)
-// showTypeOf(service1)
-// showTypeOf(service2)
-showTypeOf(servicePrice1)
-showTypeOf(servicePrice2)
-showTypeOf(rollback)
-showTypeOf(servicePercentPrice)
-showTypeOf(getRollBackMessage(fullPrice))
+showTypeOf(getTitleLet);
+showTypeOf(screenPrice);
+// showTypeOf(adaptive);
+// showTypeOf(service1);
+// showTypeOf(service2);
+showTypeOf(servicePrice1);
+showTypeOf(servicePrice2);
+showTypeOf(rollback);
+showTypeOf(servicePercentPrice);
+showTypeOf(getRollBackMessage(fullPrice));
