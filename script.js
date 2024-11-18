8aaaabbe7119fa776bd1da4e0aa5eca0e5d30cf8
Худойкулов Суннат <khudoykulov07@bk.ru>
2024-11-18 17:17:19 +0500

    Начал

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
 let gameStart = +prompt("Игра 'Угадай число' Я загадал число от 1 до 100. Попробуй угадать!")
+const isNumber = function (num) {
+	return !isNaN(parseFloat(num)) && isFinite(num);
+}
