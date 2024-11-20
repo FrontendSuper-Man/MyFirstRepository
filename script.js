536d8f484f5fe52e23c0f34666a1488de5a2cb2b
Худойкулов Суннат <khudoykulov07@bk.ru>
2024-11-20 14:24:09 +0500

    Сделал классную с Ильёй lesson08

--- script.js
+++ script.js
@@ -1,50 +1,73 @@
 const appData = {
 	title: '',
-	screens: '',
+	screens: [],
 	screenPrice: 0,
 	adaptive: true,
 	rollback: 10,
 	allServicePrices: 0,
 	fullPrice: 0,
 	servicePercentPrice: 0,
-	service1: '',
-	service2: '',
+	services: {},
+
+	start: function () {
+		this.asking();
+		this.addPrices()
+		this.getFullPrice();
+		this.getServicePercentPrices();
+		this.getTitle();
+		this.logger();
+	},
+
+	isNumber: function (num) {
+		return !isNaN(parseFloat(num)) && isFinite(num);
+	},
+
 	asking: function () {
 		this.title = prompt("Как называется ваш проект?", "Проект");
-		this.screens = prompt("Какие типы экранов нужно разработать?", "Простые");
 
-		do {
-			this.screenPrice = prompt("Сколько будет стоить данная работа?", "20000");
-		} while (!this.isNumber(this.screenPrice));
+		for (let i = 0; i < 2; i++) {
+			let name = prompt("Какие типы экранов нужно разработать?", "Простые");
+			let price = 0;
+			do {
+				price = +prompt("Сколько будет стоить данная работа?", "20000");
+			} while (!this.isNumber(price));
+			this.screens.push({ id: i, name, price, })
+		}
+
+		for (let i = 0; i < 2; i++) {
+			let name = prompt("Какой дополнительный тип услуг нужен?");
+			let price = 0;
+			do {
+				price = +prompt("Сколько это будет стоить?", 2000);
+			} while (!this.isNumber(price));
+			this.services[name] = +price
+		}
 
 		this.screenPrice = parseFloat(this.screenPrice);
 		this.adaptive = confirm("Нужен ли адаптив на сайте?");
 	},
-	isNumber: function (num) {
-		return !isNaN(parseFloat(num)) && isFinite(num);
-	},
-	getAllServicePrices: function () {
-		let sum = 0;
 
-		for (let i = 0; i < 2; i++) {
-			let service = prompt('Какой дополнительный тип услуг нужен?');
-			if (service === null) {
-				continue;
-			}
-			let servicePrice = 0;
-			while (true) {
-				servicePrice = prompt("Сколько это будет стоить?", '2000');
-				if (servicePrice === null) {
-					break;
-				}
-				if (this.isNumber(servicePrice)) {
-					sum += parseFloat(servicePrice); // Преобразуем в число
-					break;
-				}
-			}
+	addPrices: function () {
+		for (const screen of appData.screens) {
+			appData.screenPrice += screen.price
 		}
-		return sum;
+		for (const key in this.services) {
+			this.allServicePrices += this.services[key]
+		}
 	},
+
+	getFullPrice: function () {
+		this.fullPrice = this.screenPrice + this.allServicePrices;
+	},
+
+	getTitle: function () {
+		this.title = this.title.trim()[0].toUpperCase() + this.title.trim().substring(1).toLowerCase();
+	},
+
+	getServicePercentPrices: function () {
+		this.servicePercentPrice = this.fullPrice - (this.fullPrice * (this.rollback / 100));
+	},
+
 	getRollBackMessage: function (price) {
 		if (price >= 30000) {
 			return "Даём скидку в 10%";
@@ -56,15 +79,7 @@ const appData = {
 			return "Что-то пошло не так";
 		}
 	},
-	getFullPrice: function (elem1, elem2) {
-		return elem1 + elem2;
-	},
-	getTitle: function (elem) {
-		return elem.trim()[0].toUpperCase() + elem.trim().substring(1).toLowerCase();
-	},
-	getServicePercentPrices: function () {
-		return this.fullPrice - (this.fullPrice * (this.rollback / 100));
-	},
+
 	logger: function () {
 		for (const i in this) {
 			if (typeof this[i] !== "function") {
@@ -74,14 +89,7 @@ const appData = {
 			}
 		}
 	},
-	start: function () {
-		this.asking();
-		this.allServicePrices = this.getAllServicePrices();
-		this.fullPrice = this.getFullPrice(this.screenPrice, this.allServicePrices);
-		this.servicePercentPrice = this.getServicePercentPrices();
-		this.title = this.getTitle(this.title);
-		this.logger();
-	}
 };
 
 appData.start();
+console.log(appData.screens);
