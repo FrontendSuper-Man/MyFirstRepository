1db5538cb017c0df057270b61e98a003f8dfaaaf
Худойкулов Суннат <khudoykulov07@bk.ru>
2024-12-04 16:14:04 +0500

    Вот исправил бтнс и интпуты

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const h1Collection = document.getElementsByTagName('h1');
 const firstH1 = h1Collection[0];
 
-const btnsDiv = document.querySelector('.main-total > .main-total__buttons')
-const btns = btnsDiv.getElementsByClassName('handler_btn')
+const btns = document.getElementsByClassName('handler_btn')
+const btn1 = btns[0]
+const btn2 = btns[1]
 
 const plus = document.querySelector('.screen-btn')
 const other1 = document.querySelectorAll(".other-items.percent");
@@ -13,20 +14,29 @@ const typeInput = document.querySelector('.rollback input[type="range"]');
 const span = document.querySelector('.rollback span.range-value')
 
 const inputs = document.getElementsByClassName('total-input')
+const input1 = inputs[0];
+const input2 = inputs[1];
+const input3 = inputs[2];
+const input4 = inputs[3];
+const input5 = inputs[4];
 
 let screens = document.querySelectorAll(".screen")
 
-console.log(btns);
-
-// console.log(h1Collection);
-// console.log(firstH1);
-// console.log(plus);
-// console.log(other1);
-// console.log(other2);
-// console.log(typeInput);
-// console.log(span);
-// console.log(inputs);
-// console.log(screens);
+console.log(btn1);
+console.log(btn2);
+console.log(h1Collection);
+console.log(firstH1);
+console.log(plus);
+console.log(other1);
+console.log(other2);
+console.log(typeInput);
+console.log(span);
+console.log(input1);
+console.log(input2);
+console.log(input3);
+console.log(input4);
+console.log(input5);
+console.log(screens);
 
 
 
