5f5eba63ae753f306b0ea8aecba3c8e3e7a86e1e
Худойкулов Суннат <khudoykulov07@bk.ru>
2024-12-13 21:57:01 +0500

    Сделал 16

--- constructor/script.js
+++ constructor/script.js
@@ -1,37 +1,15 @@
-function DomElement(selector, height, width, bg, fontSize) {
-	this.selector = selector;
-	this.height = height;
-	this.width = width;
-	this.bg = bg;
-	this.fontSize = fontSize;
-
-	this.createElement = function () {
-		let element;
-		if (this.selector.trim().startsWith('.')) {
-			element = document.createElement('div');
-			element.className = this.selector.slice(1);
-		} else if (this.selector.trim().startsWith('#')) {
-			element = document.createElement('p');
-			element.id = this.selector.slice(1);
-		} else {
-			alert('Некорректный селектор. Используйте точку или решетку.')
-		}
-		element.style.cssText = `
-		 height: ${this.height};
-		 width: ${this.width};
-		 background: ${this.bg};
-		 font-size: ${this.fontSize};
-	  `;
-		element.textContent = 'Это созданный элемент.';
-		document.body.before(element);
-	};
+class First {
+	hello() {
+		console.log("Привет, я метод родителя!");
+	}
 }
 
-const color = 'red'
-const width = '100px'
-const height = '200px'
-const fZ = '16px'
-const selector = ' .block'
-// const selector = '#block'
-const newDomElement = new DomElement(selector, height, width, color, fZ);
-newDomElement.createElement();
+class Second extends First {
+	hello() {
+		super.hello();
+		console.log("А я наследуемый метод!");
+	}
+}
+
+const instance = new Second();
+instance.hello();
